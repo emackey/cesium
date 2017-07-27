7b173f99ea500fdd51c45f009860c0eb85bdb661
Ed Mackey <elm19087@gmail.com>
2017-07-27 15:40:29 -0400

    Remove extra light scale.

--- Source/ThirdParty/GltfPipeline/processPbrMetallicRoughness.js
+++ Source/ThirdParty/GltfPipeline/processPbrMetallicRoughness.js
@@ -487,7 +487,7 @@ define([
         fragmentShader += '    vec3 v = -normalize(v_positionEC);\n';
 
         // Generate fragment shader's lighting block
-        fragmentShader += '    vec3 lightColor = vec3(1.0, 1.0, 1.0) * 2.0;\n';
+        fragmentShader += '    vec3 lightColor = vec3(1.0, 1.0, 1.0);\n';
         if (optimizeForCesium) {
             fragmentShader += '    vec3 l = normalize(czm_sunDirectionEC);\n';
         } else {
